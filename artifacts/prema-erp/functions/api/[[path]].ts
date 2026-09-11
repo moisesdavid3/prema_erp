9f88adf1c27b58d923a97886d49266dcfbf7c907
moisesdavid3 <moisesdavid3@users.noreply.github.com>
2026-09-11 11:59:27 -0500

    Revert "Usar una sola conexión persistente en el API para evitar agotar el session pooler"
    This reverts commit 918b5c8b2d8b4c3e377271c8b3116ecbe026b3e6.

--- artifacts/prema-erp/functions/api/[[path]].ts
+++ artifacts/prema-erp/functions/api/[[path]].ts
@@ -130,7 +130,7 @@ let sharedDb: DbHandle | undefined;
 
 function getSharedDb(): DbHandle {
   if (!sharedDb) {
-    sharedDb = createDbConnection({ max: 1, idleTimeout: 0, maxLifetime: 0 });
+    sharedDb = createDbConnection({ max: 5, idleTimeout: 60, maxLifetime: 300 });
   }
   return sharedDb;
 }
